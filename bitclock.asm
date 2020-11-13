570931864d84a4ee4ddff20624e5adb88633b269
Xaver Lopenstedt <xaver@lopenstedt.de>
2020-11-13 15:16:44 +0100

    tick tack seconds

--- bitclock.asm
+++ bitclock.asm
@@ -1,7 +1,15 @@
 	processor 6502
 	include ../vcs.h
 	include ../macro.h
-	org $F000
+
+    seg.u vars
+    org $80
+hours   DS.B 1
+minutes DS.B 1
+seconds DS.B 1
+frames  DS.B 1
+    seg code_main
+    org $F000
 
 Start
 	SEI	
@@ -47,12 +55,17 @@ ClearMem
     NOP
     NOP
 
-    STA RESP0
+    STA RESP1
 
     Sleep 12
 
-    STA RESP1
+    STA RESP0
 MainLoop
+    LDA seconds
+    STA GRP0
+    LDA minutes
+    STA GRP1
+
     LDA #$02
     STA VSYNC
     
@@ -79,11 +92,29 @@ Display
     DEX
     BNE Display
 
-    LDX #36 ;; overscan
+    LDX #36 ;; overscan 36
 OverScanLoop
     STA WSYNC
     DEX
     BNE OverScanLoop
+
+    LDA frames
+    ADC #1
+    STA frames
+    CMP #50 ; PAL has 50 frames / sec
+    BNE MainLoop
+    LDA #0
+    STA frames
+    LDA seconds
+    ADC #1
+    CMP #60
+    STA seconds
+    BNE MainLoop
+    LDA #0
+    STA seconds
+    LDA minutes
+    ADC #1
+    STA minutes
 
     JMP MainLoop
 
